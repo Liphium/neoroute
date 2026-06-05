7015e8b3edbbebfd095a7a811d2ee34d0d5ef590
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-06-05 13:21:04 +0200

    feat: add testing util

---
+++ testing_adapter.go
@@ -0,0 +1,111 @@
+package neoroute
+
+import (
+	"fmt"
+	"slices"
+	"sync"
+
+	"github.com/tinylib/msgp/msgp"
+)
+
+type TestingAdapter struct {
+	eventRegistries []*EventRegistry
+	mutex           *sync.Mutex
+	sendMutex       *sync.Mutex
+	removeFunc      func()
+	closed          bool
+	transporterType string
+	removeOnce      sync.Once
+
+	// Stored events that are sent to the adapter. Used for testing purposes.
+	receivedEvents [][]byte
+}
+
+func NewTestingAdapter(eventRegistries []*EventRegistry) Adapter {
+	adapter := &TestingAdapter{
+		transporterType: "TestingAdapter",
+		eventRegistries: eventRegistries,
+		mutex:           &sync.Mutex{},
+		sendMutex:       &sync.Mutex{},
+	}
+	return adapter
+}
+
+func (a *TestingAdapter) GetEvents() ([]event, error) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	return a.unmarshalEvents()
+}
+
+func (a *TestingAdapter) DrainEvents() ([]event, error) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	events, err := a.unmarshalEvents()
+	if err != nil {
+		return nil, err
+	}
+	a.receivedEvents = nil
+	return events, nil
+}
+
+func (a *TestingAdapter) unmarshalEvents() ([]event, error) {
+
+	// Unmarshal events into struct
+	events := []event{}
+	for _, eventBytes := range a.receivedEvents {
+		var event event
+		unmarshaler := any(&event).(msgp.Unmarshaler)
+		_, err := unmarshaler.UnmarshalMsg(eventBytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal events: %v", err)
+		}
+		events = append(events, event)
+	}
+	return events, nil
+}
+
+func (a *TestingAdapter) send(b []byte) error {
+	a.sendMutex.Lock()
+	defer a.sendMutex.Unlock()
+	a.receivedEvents = append(a.receivedEvents, b)
+	return nil
+}
+
+func (a *TestingAdapter) isEventRegistered(name string) bool {
+	for _, eventRegistry := range a.eventRegistries {
+		if slices.Contains(eventRegistry.getEvents(), name) {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *TestingAdapter) getTransportType() string {
+	return a.transporterType
+}
+
+func (a *TestingAdapter) setRemoveFunc(removeFunc func()) {
+	if removeFunc == nil {
+		return
+	}
+
+	a.mutex.Lock()
+	a.removeFunc = removeFunc
+	closed := a.closed
+	a.mutex.Unlock()
+
+	if closed {
+		a.removeOnce.Do(removeFunc)
+	}
+}
+
+func (a *TestingAdapter) Close() {
+	a.mutex.Lock()
+	a.closed = true
+	removeFunc := a.removeFunc
+	a.mutex.Unlock()
+
+	if removeFunc != nil {
+		a.removeOnce.Do(removeFunc)
+	}
+}
---
+++ testing_context.go
@@ -0,0 +1,84 @@
+package neoroute
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/tinylib/msgp/msgp"
+)
+
+func NewTestingResCtx[D any, RS any, PS interface {
+	*RS
+	msgp.Marshaler
+}](neo *NeoRouter[D], route string, session *Session[D]) *ResCtx[D, RS, PS] {
+	return &ResCtx[D, RS, PS]{
+		Ctx: *NewTestingCtx[D](neo, route, session),
+	}
+}
+
+func NewTestingOkCtx[D any](neo *NeoRouter[D], route string, session *Session[D]) *OkCtx[D] {
+	return &OkCtx[D]{
+		Ctx: *NewTestingCtx[D](neo, route, session),
+	}
+}
+
+func NewTestingCtx[D any](neo *NeoRouter[D], route string, session *Session[D]) *Ctx[D] {
+	return &Ctx[D]{
+		neo:     neo,
+		id:      1,
+		route:   route,
+		session: session,
+	}
+}
+
+func NewTestingSession[D any](data D, sessionId string) *Session[D] {
+	return &Session[D]{
+		sessionData: data,
+		id:          sessionId,
+		mutex:       &sync.Mutex{},
+	}
+}
+
+func GetTestingResponse[RQ any, PQ interface {
+	*RQ
+	msgp.Unmarshaler
+}](err error) (RQ, string, error) {
+	var respErr response
+	var resp RQ
+	if errors.As(err, &respErr) {
+		if respErr.IsError {
+			return resp, string(respErr.Data), nil
+		}
+		if !respErr.HasData {
+			return resp, "", fmt.Errorf("response has no data")
+		}
+
+		// Unmarshal response data into struct
+		unmarshaler := any(&resp).(msgp.Unmarshaler)
+		_, err := unmarshaler.UnmarshalMsg(respErr.Data)
+		if err != nil {
+			return resp, "", fmt.Errorf("failed to unmarshal response data: %v", err)
+		}
+		return resp, "", nil
+	} else {
+		return resp, "", fmt.Errorf("response is not response type")
+	}
+}
+
+// GetTestingResponseOk return the error message or an error from the handler or an error if the response is not correct.
+func GetTestingResponseOk(err error) (string, error) {
+	var respErr response
+
+	if errors.As(err, &respErr) {
+		if respErr.IsError {
+			return string(respErr.Data), nil
+		}
+		if respErr.HasData {
+			return "", fmt.Errorf("ok response without error should not have any data")
+		}
+		return "", nil
+	} else {
+		return "", fmt.Errorf("response is not response type")
+	}
+}
