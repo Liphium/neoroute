b32b8c53c6e68e2f2b0cdd82867a8724ef87cb7e
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-06-13 18:14:48 +0200

    feat: update context to make it easier to use

--- context.go
+++ context.go
@@ -2,12 +2,75 @@ package neoroute
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/tinylib/msgp/msgp"
 )
 
-// Wraps context for handlers that return a response.
+// Context allows helper functions to accept Ctx, ResCtx, or
+// OkCtx interchangeably to extract their underlying data.
+type Context[D any] interface {
+	BaseCtx() *Ctx[D] // BaseCtx returns the underlying Ctx, allowing access to the session, request data, etc.
+}
+
+// Responder can be used to typecast a Context to check
+// if it supports error responding.
+type Responder interface {
+	RespondCustom(error) error // Uses ErrorHandler function to convert an error to a error response string.
+	RespondError(string) error // Sends an error response with the specified message.
+}
+
+// --------------------------------------------------------------------------------
+// Base Context
+// --------------------------------------------------------------------------------
+
+type Ctx[D any] struct {
+	neo      *NeoRouter[D]
+	id       int         // request id, used for responses
+	reqData  []byte      // data field from Request struct
+	route    string      // the route that matched the request
+	session  *Session[D] // clients session, contains the session data and id
+	runAfter []func()    // functions to run after the handler finishes, used for cleanup
+}
+
+func (c *Ctx[D]) BaseCtx() *Ctx[D] {
+	return c
+}
+
+func (c *Ctx[D]) Id() int {
+	return c.id
+}
+
+func (c *Ctx[D]) Route() string {
+	return c.route
+}
+
+func (c *Ctx[D]) Session() *Session[D] {
+	return c.session
+}
+
+// RunAfter allows handlers to register functions that will be executed after the response is sent.
+func (c *Ctx[D]) RunAfter(fn func(), fns ...func()) *Ctx[D] {
+	c.runAfter = append(c.runAfter, fn)
+	if len(fns) > 0 {
+		c.runAfter = append(c.runAfter, fns...)
+	}
+	return c
+}
+
+// unexported package helper so ResCtx and OkCtx can share code without
+// exposing response logic on the public Ctx API surface.
+func (c *Ctx[D]) respondError(err string) response {
+	return response{
+		Id:      c.id,
+		HasData: true,
+		IsError: true,
+		Data:    []byte(err),
+	}
+}
+
+// --------------------------------------------------------------------------------
+// Response Context
+// --------------------------------------------------------------------------------
 
 type ResCtx[D any, RS any, PS interface {
 	*RS
@@ -16,9 +79,12 @@ type ResCtx[D any, RS any, PS interface {
 	*Ctx[D]
 }
 
-func (c *ResCtx[D, RS, PS]) Respond(resp RS) error {
+func (c *ResCtx[D, RS, PS]) BaseCtx() *Ctx[D] {
+	return c.Ctx
+}
 
-	// Marshal response data
+// Respond sends a successful response with the provided data.
+func (c *ResCtx[D, RS, PS]) Respond(resp RS) error {
 	marshaler := any(&resp).(msgp.Marshaler)
 	respData, err := marshaler.MarshalMsg(nil)
 	if err != nil {
@@ -33,20 +99,29 @@ func (c *ResCtx[D, RS, PS]) Respond(resp RS) error {
 	}
 }
 
+// RespondError sends an error response with the specified message.
 func (c *ResCtx[D, RS, PS]) RespondError(err string) error {
 	return c.respondError(err)
 }
 
+// RespondCustom uses ErrorHandler function to convert an error to a error response string.
 func (c *ResCtx[D, RS, PS]) RespondCustom(err error) error {
 	return c.respondError(c.neo.config.ErrorHandler(err))
 }
 
-// Wraps context for handlers that don't return any data, only success or error.
+// -----------------------------------------------------------------------------
+// OK Context (Used by RouteOk / RouteOkNoRequest)
+// -----------------------------------------------------------------------------
 
 type OkCtx[D any] struct {
 	*Ctx[D]
 }
 
+func (c *OkCtx[D]) BaseCtx() *Ctx[D] {
+	return c.Ctx
+}
+
+// RespondOk sends a successful response.
 func (c *OkCtx[D]) RespondOk() error {
 	return response{
 		Id:      c.id,
@@ -56,48 +131,12 @@ func (c *OkCtx[D]) RespondOk() error {
 	}
 }
 
+// RespondError sends an error response with the specified message.
 func (c *OkCtx[D]) RespondError(err string) error {
 	return c.respondError(err)
 }
 
+// RespondCustom uses ErrorHandler function to convert an error to a error response string.
 func (c *OkCtx[D]) RespondCustom(err error) error {
 	return c.respondError(c.neo.config.ErrorHandler(err))
-}
-
-// Default context that contains the request information.
-
-type Ctx[D any] struct {
-	neo      *NeoRouter[D]
-	id       int    // request id, used for responses
-	reqData  []byte // data field from Request struct
-	route    string // the route that matched the request
-	session  *Session[D]
-	runAfter []func() // functions to run after the handler finishes, used for cleanup
-}
-
-func (c Ctx[D]) Id() int {
-	return c.id
-}
-
-func (c Ctx[D]) Route() string {
-	return c.route
-}
-
-func (c Ctx[D]) Session() *Session[D] {
-	return c.session
-}
-
-func (c *Ctx[D]) RunAfter(fn func(), fns ...func()) *Ctx[D] {
-	c.runAfter = append(c.runAfter, append([]func(){fn}, fns...)...)
-	log.Println("length", len(c.runAfter))
-	return c
-}
-
-func (c Ctx[D]) respondError(err string) response {
-	return response{
-		Id:      c.id,
-		HasData: true,
-		IsError: true,
-		Data:    []byte(err),
-	}
 }
