54e9e2f48854d8f23b149d8d2c51b6337f93fe6d
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-06-14 17:30:36 +0200

    feat: add new function that allows users to update session data concurrency safe

--- session.go
+++ session.go
@@ -25,8 +25,20 @@ func (s *Session[D]) Data() D {
 	return s.sessionData
 }
 
+// SetData allows you to set the sessions data.
 func (s *Session[D]) SetData(data D) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 	s.sessionData = data
+}
+
+// UpdateData allows you to update the sessions data.
+// While the updateFunc is running, the session will be locked,
+// so other goroutines will not be able to access the session data until the updateFunc is done.
+func (s *Session[D]) UpdateData(updateFunc func(data *D)) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	updateData := &s.sessionData
+	updateFunc(updateData)
+	s.sessionData = *updateData
 }
