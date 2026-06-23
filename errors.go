45830ed059da8326091f9763efb9d145694ff668
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-06-23 13:56:30 +0200

    feat(neoroute): add new errors

--- errors.go
+++ errors.go
@@ -2,6 +2,8 @@ package neoroute
 
 const (
 	ErrRouteNotExists       = "route does not exist"
+	ErrHandshakeFailed      = "handshake failed"
+	ErrReadingBody          = "failed to read request body"
 	ErrInvalidRequestFormat = "invalid request format"
 	ErrMiddlewareDenied     = "middleware denied the request"
 )
