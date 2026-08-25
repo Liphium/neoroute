cb6ab869654918bbab5659b38e797a8195c72d19
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-08-25 21:40:48 +0200

    test: new test cases for Router Init

--- router_test.go
+++ router_test.go
@@ -6,13 +6,6 @@ import (
 	"testing"
 )
 
-const (
-	routingTypeGroup     = "group"
-	routingTypeRoute     = "route"
-	routingTypeAddRouter = "add-router"
-	routingTypeUse       = "use"
-)
-
 func TestRouter_Init(t *testing.T) {
 	middlewareFunc := func(c *Ctx[NoData]) bool { return false }
 	tests := []struct {
@@ -21,11 +14,118 @@ func TestRouter_Init(t *testing.T) {
 		want        map[string]int // route -> middleware amount
 	}{
 		{
+			name: "no routes, no middleware",
+			routingFunc: func(r *Router[NoData]) {
+			},
+			want: map[string]int{},
+		},
+		{
+			name: "single route",
+			routingFunc: func(r *Router[NoData]) {
+				r.RoutePing("some-route", func(c *Ctx[NoData]) {})
+			},
+			want: map[string]int{"some-route": 0},
+		},
+		{
 			name: "only use middleware, with no route",
 			routingFunc: func(r *Router[NoData]) {
 				r.Use("some-route", middlewareFunc)
 			},
 			want: map[string]int{},
+		},
+		{
+			name: "route with middleware",
+			routingFunc: func(r *Router[NoData]) {
+				r.RoutePing("some-route", func(c *Ctx[NoData]) {}).Use("", middlewareFunc)
+			},
+			want: map[string]int{"some-route": 1},
+		},
+		{
+			name: "route with multiple middlewares",
+			routingFunc: func(r *Router[NoData]) {
+				r.RoutePing("some-route", func(c *Ctx[NoData]) {}).Use("", middlewareFunc).Use("", middlewareFunc)
+			},
+			want: map[string]int{"some-route": 2},
+		},
+		{
+			name: "router a group and two routes one has a middleware",
+			routingFunc: func(r *Router[NoData]) {
+				group := r.Group("some-group")
+				group.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				group.RoutePing("some-route2", func(c *Ctx[NoData]) {}).Use("", middlewareFunc)
+			},
+			want: map[string]int{"some-group/some-route1": 0, "some-group/some-route2": 1},
+		},
+		{
+			name: "middleware on group, 2 routes and one has a middleware",
+			routingFunc: func(r *Router[NoData]) {
+				group := r.Group("some-group")
+				group.Use("", middlewareFunc)
+				group.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				group.RoutePing("some-route2", func(c *Ctx[NoData]) {}).Use("", middlewareFunc)
+			},
+			want: map[string]int{"some-group/some-route1": 1, "some-group/some-route2": 2},
+		},
+		{
+			name: "middleware on group applied at the end, 2 routes and one has a middleware",
+			routingFunc: func(r *Router[NoData]) {
+				group := r.Group("some-group")
+				group.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				group.RoutePing("some-route2", func(c *Ctx[NoData]) {}).Use("", middlewareFunc)
+				group.Use("", middlewareFunc)
+			},
+			want: map[string]int{"some-group/some-route1": 1, "some-group/some-route2": 2},
+		},
+		{
+			name: "middleware on subroute",
+			routingFunc: func(r *Router[NoData]) {
+				r.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				r.Use("some-route1", middlewareFunc)
+			},
+			want: map[string]int{"some-route1": 1},
+		},
+		{
+			name: "add route to router, then add it to base router",
+			routingFunc: func(r *Router[NoData]) {
+				router := NewRouter(Config[NoData]{})
+				router.RoutePing("some-route", func(c *Ctx[NoData]) {})
+				r.AddRouters(router)
+			},
+			want: map[string]int{"some-route": 0},
+		},
+		{
+			name: "add sophisticated router to base router",
+			routingFunc: func(r *Router[NoData]) {
+				router := NewRouter(Config[NoData]{})
+				router.RoutePing("some-route", func(c *Ctx[NoData]) {})
+				group := router.Group("some-group")
+				group.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				group.Use("", middlewareFunc)
+				r.AddRouters(router)
+			},
+			want: map[string]int{"some-route": 0, "some-group/some-route1": 1},
+		},
+		{
+			name: "stack multiple routers",
+			routingFunc: func(r *Router[NoData]) {
+				router := NewRouter(Config[NoData]{})
+				router2 := NewRouter(Config[NoData]{})
+				router3 := NewRouter(Config[NoData]{})
+				group := router2.Group("some-group")
+				router3.RoutePing("some-route1", func(c *Ctx[NoData]) {})
+				router3.Use("", middlewareFunc)
+				group.AddRouters(router3)
+				r.AddRouters(router, router2)
+			},
+			want: map[string]int{"some-group/some-route1": 1},
+		},
+		{
+			name: "middleware on a group and a route with same name",
+			routingFunc: func(r *Router[NoData]) {
+				r.Group("group1").Use("", middlewareFunc)
+				r.RoutePing("group1", func(c *Ctx[NoData]) {})
+			},
+			want: map[string]int{"group1": 0},
 		},
 	}
 	for _, tt := range tests {
