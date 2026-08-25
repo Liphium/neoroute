5d87a8d09559fe5a1e73be04d75917a54919aaf7
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-08-25 12:00:46 +0200

    test: add test case for cleanRoute test

--- util_test.go
+++ util_test.go
@@ -43,6 +43,11 @@ func Test_cleanRoute(t *testing.T) {
 			route: "",
 			want:  "",
 		},
+		{
+			name:  "just a route separator",
+			route: "" + string(RouteSeparator) + "",
+			want:  "",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
