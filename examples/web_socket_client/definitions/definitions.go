bae2e2e5a8d9a092266b0b60f03d273b3ab35a5a
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-08-27 19:37:59 +0200

    docs: remove old examples

--- examples/web_socket_client/definitions/definitions.go
+++
@@ -1,6 +0,0 @@
-package definitions
-
-// This generates definitions for your server automatically through neogen.
-// We recommend using this in supported languages for type-safety.
-
-//go:generate neogen -path ../../web_socket_server
