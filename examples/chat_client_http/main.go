eba2b33c58c5ece30ae08ea74f06fd0ea1c8201c
Tiemingo <212736953+tiemingo@users.noreply.github.com>
2026-08-27 13:30:10 +0200

    docs: add chat client http example

---
+++ examples/chat_client_http/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"chatclienthttp/generated"
+	"fmt"
+	"log/slog"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/Liphium/neoroute/client"
+)
+
+func main() {
+	if length := len(os.Args); length < 2 || length > 3 || (length == 2 && os.Args[1] == "--help") {
+		fmt.Println("Usage: go run . <message> <name  (optional)>")
+		return
+	}
+
+	message := os.Args[1]
+	name := ""
+	if len(os.Args) == 3 {
+		name = os.Args[2]
+	}
+
+	u, _ := url.Parse("http://localhost:6121/http")
+
+	// Create a new HTTP connector from the generated code
+	httpConn := generated.NewHttpConnector(client.Config{
+		ErrorHandler: func(err error) {
+			slog.Error("error", "message", err)
+			os.Exit(1)
+		},
+		RequestTimeout: 10 * time.Second,
+	}, "GET", u)
+
+	// Use the generated connector to send the message
+	sendErr := httpConn.SendSend(generated.SendRequest{
+		Sender: name,
+		Text:   message,
+	})
+	if sendErr != nil {
+		slog.Error("error", "message", sendErr)
+		os.Exit(1)
+	}
+}
